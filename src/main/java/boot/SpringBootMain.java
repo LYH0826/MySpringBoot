d7bfb95afd0d33c0dcf56d7aa71787d40babe33e
LYH <1140527147@qq.com>
2021-11-09 13:49:48 +0800

    idea test commit1

--- src/main/java/boot/SpringBootMain.java
+++ src/main/java/boot/SpringBootMain.java
@@ -117,5 +117,6 @@ public class SpringBootMain {
         System.out.println("I am hot-fix");
         System.out.println("I am master");
         System.out.println("I am hot-fix2");
+        System.out.println("IDEA push new code!");
     }
 }
